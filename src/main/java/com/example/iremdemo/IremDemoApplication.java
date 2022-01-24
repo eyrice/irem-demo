5875b12257c04d9fb8bc2f5dfba9a43dc15a9d7e
eyrice <95687333+eyrice@users.noreply.github.com>
2022-01-24 11:30:59 +0300

    first commit

---
+++ src/main/java/com/example/iremdemo/IremDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.iremdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IremDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IremDemoApplication.class, args);
+	}
+
+}
